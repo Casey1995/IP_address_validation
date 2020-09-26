96fa36e402b4e184b257639d8ead39318ee693e0
Casey1995 <54296795+Casey1995@users.noreply.github.com>
2020-09-26 13:00:19 -0400

    Update checkIP.py

--- checkIP.py
+++ checkIP.py
@@ -1,15 +1,14 @@
 import re
 def checkIP():
-    number = input('Enter what is displayed: ')
-    #while True:
+    number = input('Enter your IP address: ')
     ipv4 = re.search('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})', number)
     ipv6 = re.search('(([a-f0-9:]+:+)+[a-f0-9]+)', number)
     if ipv4:
-        print('4')
+        print('This is version 4')
     elif ipv6:
-        print('6')
+        print('This is version 6')
     else:
-        print('-1')
+        print('Invalid IP')
 checkIP()
 
 #This is a second function.
