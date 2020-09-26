c0e93ccc043bd586cd3d64d1c31507592f09caf7
Casey1995 <54296795+Casey1995@users.noreply.github.com>
2020-09-26 13:02:19 -0400

    Update checkIP.py

--- checkIP.py
+++ checkIP.py
@@ -1,6 +1,7 @@
 import re
 def checkIP():
     number = input('Enter your IP address: ')
+    # Alternate IPv4 regex. ipv4 = re.search('(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})', number)
     ipv4 = re.search('([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})', number)
     ipv6 = re.search('(([a-f0-9:]+:+)+[a-f0-9]+)', number)
     if ipv4:
